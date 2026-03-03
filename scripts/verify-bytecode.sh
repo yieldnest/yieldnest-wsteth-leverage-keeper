30ecfe7c28a07f036ec5d59f2d671fb4dc09feea
danoctavian <danoctavian91@gmail.com>
2026-03-03 03:55:48 +0200

    new deployment

--- scripts/verify-bytecode.sh
+++ scripts/verify-bytecode.sh
@@ -5,21 +5,21 @@ RPC_URL="${ETH_MAINNET_RPC_URL:?Set ETH_MAINNET_RPC_URL}"
 
 echo "Verifying StablecoinRateProvider..."
 forge verify-bytecode \
-  0xe16e55f7dd4bbb4aCaeDd49c56E31289D63515DA \
+  0xeC90d3F01Ea53d4e99b1ee758F160521C2968c12 \
   src/StablecoinRateProvider.sol:StablecoinRateProvider \
   --rpc-url "$RPC_URL" \
-  --constructor-args 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
+  --constructor-args 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
 
 echo "Verifying LatestAnswerAdapter..."
 forge verify-bytecode \
-  0xfEc724764A4C8d9aBBe08158aE55BDA3F828fF46 \
+  0x537C8D5a25c5467A3A68Fc0ffDfC89eA9C309360 \
   src/LatestAnswerAdapter.sol:LatestAnswerAdapter \
   --rpc-url "$RPC_URL" \
-  --constructor-args 0x8b6851156023f4f5a66f68bea80851c3d905ac93 8
+  --constructor-args 0x8B6851156023f4f5A66F68BEA80851c3D905Ac93 8
 
 echo "Verifying FlexStrategyLeverageKeeper..."
 forge verify-bytecode \
-  0x4F0DEd2b304645749c137bFE092b9f48e5795cdE \
+  0x6920C7c9a66EdEa563b1aEcb8CA8097f811CbFc5 \
   src/FlexStrategyLeverageKeeper.sol:FlexStrategyLeverageKeeper \
   --rpc-url "$RPC_URL" \
   --constructor-args 0x67a114e733b52cac50a168f02b5626f500801c62
